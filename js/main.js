e8cc21cc34e8740cdd901fb708d21c77d93521a3
koskos1986 <ekosopletkin@yandex.ru>
2021-04-08 21:56:22 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,47 @@
+'use strict';
+(function () {
+  const header = document.querySelector('.header');
+  const leadMenu = document.querySelector('.lead');
+  const leadButton = document.querySelector('.lead__button')
+
+  const openButton = leadMenu.querySelector('.lead__button--open');
+  const closeButton = header.querySelector('.header__button--close');
+
+  header.classList.remove('header--nojs');
+  leadButton.classList.remove('lead__button--nojs');
+  header.classList.add('header--close');
+
+  const menuToggle = function (evt) {
+    if (evt.target === openButton) {
+      header.classList.toggle('header--close');
+      header.classList.toggle('header--open');
+    }
+
+    if (evt.target === closeButton) {
+      header.classList.toggle('header--close');
+      header.classList.toggle('header--open');
+    }
+  };
+
+  document.addEventListener('click', menuToggle);
+})();
+
+(function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+  for (const link of links) {
+    function handleButtonClick(event) {
+    event.preventDefault();
+      const id = link.getAttribute('href');
+      document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    }
+      link.addEventListener('click', handleButtonClick);
+  };
+}());
+
+(function () {
+  const inputElement = document.querySelector('input[type=tel]');
+  const maskOptions = {
+    mask: '+{7}(000)000-00-00'
+  };
+  IMask(inputElement, maskOptions);
+})();
